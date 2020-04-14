3d246eafba09800340983bf3267074307c85412f
WILLIAM8812 <54324549+WILLIAM8812@users.noreply.github.com>
2020-04-14 15:48:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 // Load up the discord.js library
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const prefixlol = "+"
 
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
-const client = new Discord.Client();
+const client = new Discord.client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
 // config.token contains the bot's token
