01c9d309296fc9094e527b317539c380dafea778
WILLIAM8812 <54324549+WILLIAM8812@users.noreply.github.com>
2020-04-15 14:21:35 +0200

    added node_lib

--- bot.js
+++ bot.js
@@ -204,7 +204,7 @@ if(command === "election") {
     .setTimestamp()
     message.channel.send({embed})
     .then(function (message){
-      message.react(":✔:")
+      message.react("✔")
     }).catch(function() {
     });
 
