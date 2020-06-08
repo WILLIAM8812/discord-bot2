a991596369d2b9094a367243be6df674a1f686fd
WILLIAM8812 <54324549+WILLIAM8812@users.noreply.github.com>
2020-06-08 22:53:00 +0200

    deleted mongodb

--- bot.js
+++ bot.js
@@ -2,39 +2,32 @@
 const Discord = require('discord.js');
 const prefixlol = "+"
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://William:(process.env.PASSWORD)@cluster0-0jorp.mongodb.net/Cluster0?retryWrites=true&w=majority`;
-const db = new MongoClient(uri, { useNewUrlParser: true });
-db.connect;
+const client = new Discord.Client();
 
-
-
-const self = new Discord.Client();
-
-self.on("ready", () => {
+client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${self.users.size} users, in ${self.channels.size} channels of ${self.guilds.size} guilds.`); 
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 
-  self.user.setPresence({ game: { name: '+help | by WILLIAM8812' }, status: 'online' })
+  client.user.setPresence({ game: { name: '+help | by WILLIAM8812' }, status: 'online' })
   .then(console.log)
   .catch(console.error);
 });
 
-self.on("guildCreate", guild => {
+client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 });
 
-self.on("guildDelete", guild => {
+client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
 
-self.on("message", async message => {
+client.on("message", async message => {
   // This event will run on every single message received, from any channel or DM.
   
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // It's good practice to ignore other bots. This also makes your bot ignore itclient
   // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
   
@@ -54,9 +47,9 @@ self.on("message", async message => {
   if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const bsod = self.emojis.find(emoji => emoji.name === "bsod");
+    const bsod = client.emojis.find(emoji => emoji.name === "bsod");
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! La latence est de ${m.createdTimestamp - message.createdTimestamp}ms. La latence du bot est de  ${Math.round(self.ping)}ms ${bsod}`);
+    m.edit(`Pong! La latence est de ${m.createdTimestamp - message.createdTimestamp}ms. La latence du bot est de  ${Math.round(client.ping)}ms ${bsod}`);
   }
 
   if(command === "help") {
@@ -66,7 +59,7 @@ self.on("message", async message => {
     .setColor("#FFD800")
     .setDescription(`__**help**__ : Affiche la liste d'aide \n \n __**ping**__ : Affiche le delais en ms \n \n __**say**__ : Fait dire un message specifique au bot \n \n __**purge**__ : Permet de supprimer les messages (jusqu'a 100) \n \n __**code**__ : Permet de consultée le code open source du bot ! \n \n __**ano**__ : Permet d'envoyer un message anonyme a quelqu'un \n __(+ano [Titre sans espace] [Texte])__ \n \n __**advert**__ : Permet d'envoyer une publicité \n __(+advert [Texte])__ \n \n **__vote__** : Permet de créer un vote \n __(+vote [Titre sans espace] [Texte])__ \n \n **Le préfixe est actuellemnt** ${prefixlol}`)
     .setTimestamp()
-    .setFooter(self.user.username, self.user.avatarURL)
+    .setFooter(client.user.username, client.user.avatarURL)
     message.channel.send({embed})
     
     
@@ -80,7 +73,7 @@ self.on("message", async message => {
   
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
+    // To get the "message" itclient we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
@@ -132,7 +125,7 @@ self.on("message", async message => {
    */
   .setColor([255, 0, 0])
   .setDescription(emebed_text)
-  .setFooter(self.user.username, self.user.avatarURL)
+  .setFooter(client.user.username, client.user.avatarURL)
   /*
    * Takes a Date object, defaults to current date.
    */
@@ -165,7 +158,7 @@ if(command === "advert") {
    */
   .setColor("#FFD800")
   .setDescription(advert_text)
-  .setFooter(self.user.username, self.user.avatarURL)
+  .setFooter(client.user.username, client.user.avatarURL)
   /*
    * Takes a Date object, defaults to current date.
    */
@@ -197,7 +190,7 @@ if(command === "vote") {
     .setAuthor(message.author.username, message.author.avatarURL)
     .setColor("#FFD800")
     .setDescription(election_texte)
-    .setFooter("Répondre avec ✅ ou ❌", self.user.avatarURL)
+    .setFooter("Répondre avec ✅ ou ❌", client.user.avatarURL)
     .setTimestamp()
     message.channel.send({embed})
       .then(function(message) {
@@ -207,38 +200,7 @@ if(command === "vote") {
       });
 }
 
-if(command === "info") {
-
-     db.default.find().toArray(function (error, results) {
-        if (error) throw error;
-
-        results.forEach(function(i, obj) {
-            message.reply(`
-                ID  : ${obj._id.toString()}\n
-                Nom : ${obj.name} \n       
-                Jeu : ${obj.game}              
-				`);
-        });
-    });
-
-}
-
-if(command === "set") {
-
-
-  const db_texte = args.slice(1).join(' ');
-  const db_nom = args[0];
-
-  if(!db_texte)
-    return message.reply("Erreur, veullez mettre un titre et du texte");
-
-     db.default.save(objNew, { name: db_name }, { text: db_texte }); // Ce document sera inséré
-	  
-}
-
-
-
 
 });
-self.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
 
