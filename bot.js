da45f337198fa6944dc835da5f1967028b463dd8
WILLIAM8812 <54324549+WILLIAM8812@users.noreply.github.com>
2020-07-03 18:57:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 
-  client.user.setPresence({ game: { name: '+help | by WILLIAM8812' }, status: 'online' })
+  client.user.setPresence({ game: { name: '+help | by WILLIAM8812' }, status: 'online' });
   .then(console.log)
   .catch(console.error);
 });
